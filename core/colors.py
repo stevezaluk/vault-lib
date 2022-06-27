6047f830a075b080800af5b19487cfba87a87025
Steven Zaluk <stevezaluk@gmail.com>
2022-06-27 13:39:04 -0400

    Added colored print output

---
+++ core/colors.py
@@ -0,0 +1,35 @@
+import sys
+from colorama import Fore, Style
+
+RED=Fore.RED
+GREEN=Fore.GREEN
+BLUE=Fore.BLUE
+NC=Style.RESET_ALL
+
+def print_error(*text, fatal=False):
+    str = "[{r}error{nc}] ".format(r=RED, nc=NC)
+
+    for t in text:
+        str += t
+
+    print(str)
+
+    if fatal:
+        sys.exit(1)
+
+def print_success(*text, fatal=False):
+    str = "[{g}good{nc}] ".format(g=GREEN, nc=NC)
+
+    for t in text:
+        str += t
+
+    print(str)
+
+def print_info(*text, fatal=False):
+    str = "[{b}error{nc}] ".format(b=BLUE, nc=NC)
+
+    for t in text:
+        str += t
+
+    print(str)
+

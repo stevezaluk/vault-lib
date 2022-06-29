99c1e0f0f3b136ac1e0dd23157f829d35166a3dd
Steven Zaluk <stevezaluk@gmail.com>
2022-06-29 12:47:59 -0400

    Fixed generate object method

--- media_types/__init__.py
+++ media_types/__init__.py
@@ -1,15 +1,34 @@
 import os, sys
 
+from server.section import Section
+
 from ..core.file import MediaFile, PlexFile, File
 from ..media_types.game import GameFile
+from ..media_types.book import Book
+
+from ..media_types.tv_show import TVShow, TVEpisode
 
 def generate_object(json: dict):
     keys = json.keys()
-    if "plex_info" in keys:
-        return PlexFile(json)
-    elif "media_info" in keys:
-        return MediaFile(json)
-    elif "game_info" in keys:
-        return GameFile(json)
-    elif "course_info" in keys:
-        return 
+
+    ret = None
+    for key in keys:
+        if key == "plex_info":
+            ret = PlexFile(json)
+
+        if key == "media_info":
+            ret = MediaFile(json)
+
+        if key == "tv_info":
+            ret = TVShow(json)
+
+        if key == "episode_info":
+            ret = TVEpisode(json)
+
+        if key == "game_info":
+            ret = GameFile(json)
+
+        if key == "book_info":
+            ret = Book(json)
+
+    return ret

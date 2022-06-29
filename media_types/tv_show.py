bf2623cc7c5d1889cd36c38ff66989677ddb7b80
Steven Zaluk <stevezaluk@gmail.com>
2022-06-29 12:42:52 -0400

    Simplified and regrouped similar objects

--- media_types/tv_show.py
+++ media_types/tv_show.py
@@ -1,6 +1,7 @@
 import os, sys
 
 from ..core.directory import Directory
+from ..core.file import MediaFile
 from ..errors import InvalidJSONDocument
 
 class TVShow(Directory):
@@ -18,3 +19,33 @@ class TVShow(Directory):
 	def validate(self):
 		if "tv_info" not in self.get_json().keys():
 			raise InvalidJSONDocument("JSON Document is not a TV show")
+
+class TVEpisode(MediaFile):
+	def __init__(self, json):
+		super(TVEpisode, self).__init__(json)
+
+		self.validate()
+
+		self.episode_info = self.get_json()["epsiode_info"]
+
+		self.season_number = self.episode_info["season_number"]
+		self.episode_number = self.episode_info["episode_number"]
+
+		# self.parent
+
+	def validate(self):
+		if "episode_info" not in self.get_json().keys():
+			raise InvalidJSONDocument("JSON Document is not a episode")
+
+class Anime(TVShow):
+    def __init__(self, json: dict):
+        super(Anime, self).__init__(json)
+
+        self.validate()
+        
+        self.dub_status = self.tv_info["dub_status"]
+
+    def validate(self):
+        if "dub_status" not in self.tv_info.keys():
+            raise InvalidJSONDocument("JSON Document is not a anime")
+
